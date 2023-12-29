159f3e008724ff8884b4abb211ebc5b35f94cc29
parsasstudio <parsak@uw.edu>
2023-12-29 15:01:36 -0800

    v3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,66 +1,113 @@
 import Head from 'next/head';
 import SpeakerCards from '../components/SpeakerCards';
 import speakersData from '../data/speakersData.json';
+import React from 'react';
 
 export default function Home() {
 	return (
 		<>
 			{/* <img src='/gatech-banner.png' alt='Georgia Tech Banner' /> */}
-			<div className='container mx-auto px-4 pt-4 md:px-16 xl:px-32'>
+
+			<div className='container mx-auto px-4 pt-6 md:px-16 xl:px-32'>
 				<Head>
 					<title>Turing Award Speaker Series</title>
 				</Head>
-				<div className='bg-gray-100 px-5 rounded-sm h-[90vh]'>
-					<div className='h-[100vh] flex flex-col justify-center items-center'>
-						{/* Turing Award Speaker Series Header */}
-						<header className='text-center my- max-w-4xl'>
-							<h1 className='gradient-text text-4xl md:text-6xl font-bold outline-[#a4925a] outline'>Turing Award Speaker Series</h1>
-							<p className='mt-4 text-lg md:text-xl'>
-								Join us in exploring insights from some of the world&apos;s most distinguished computer scientists, recipients of the prestigious Turing Award.
-							</p>
-						</header>
 
-						{/* Turing Award Introduction
-        <section className='text-start max-w-3xl'>
-            <h2 className='text-[#a4925a] text-xl md:text-2xl font-semibold'>What is the Turing Award?</h2>
-            <p className='text-sm md:text-base leading-relaxed'>
-                The Turing Award, often referred to as the &apos;Nobel Prize of Computing,&apos; is an annual award given by the Association for Computing Machinery (ACM) to individuals for their contributions of lasting and major technical importance to the computing field.
-            </p>
-        </section> */}
+				{/* Turing Award Speaker Series Header */}
+				<div className='bg-zinc-100 md:bg-[#668cab1f] px-5 rounded-sm h-[90vh] md:outline md:outline-[#a4925a]'>
+					<div className='h-[100vh] flex flex-col justify-center items-center pb-16'>
+						{/* Turing Award Speaker Series Header */}
+						<header className='flex flex-col text-center max-w-4xl  drop-shadow-[0_0.2px_0.8px_#a4925a]'>
+							<div className='flex justify-center py-4 text-4xl md:text-6xl font-bold'>
+								<h1 className='animated-gradient-text'>
+									Turing Award<div className='pb-3 gradient-text'> Speaker Series</div>
+								</h1>
+							</div>
+							<div>
+								<p className='ibm-plex-mono flex justify-center text-sm md:text-base italic max-w-3xl'>
+									The Turing Award, often referred to as the &apos;Nobel Prize of Computing,&apos; is an annual award given by the Association for Computing Machinery (ACM) to
+									individuals for their contributions of lasting and major technical importance to the computing field.
+								</p>
+							</div>
+							<div className='flex justify-center py-8 space-x-5'>
+								<a href='#speakerCards' className='outline-[#a4925a] outline px-3 py-3 italic underline'>
+									RSVP NOW
+								</a>
+								<a href='#speakerCards' className='outline-[#a4925a] outline px-3 py-3 italic'>
+									FAQ
+								</a>
+								<a href='#contact' className='outline-[#a4925a] outline px-3 py-3 italic'>
+									CONTACT
+								</a>
+							</div>
+							{/* <p className='text-sm md:text-base italic max-w-2xl'>
+								Georgia Tech OMSCS is proud to host a series of talks by Turing Award winners. The talks will be open to the public and will be held online via Zoom.
+							</p> */}
+						</header>
 					</div>
 				</div>
 
-				<section className='my-12 grid grid-cols-1 md:grid-cols-2 gap-4'>
+				<section id='speakerCards' className='my-12 grid grid-cols-1 md:grid-cols-2 gap-4'>
 					{speakersData.map((speaker, index) => (
 						<SpeakerCards key={index} {...speaker} />
 					))}
 				</section>
 
+				{/* FAQ Section */}
+				<section id='FAQ' className='my-12'>
+					<h2 className='text-[#a4925a] text-3xl font-semibold'>Frequently Asked Questions</h2>
+					<div className='mt-4 space-y-6'>
+						<div className='space-y-2'>
+							<p className='italic ibm-plex-mono'>Where will the event take place?</p>
+							<p className='ibm-plex-mono'>The event will be held online via Teams.</p>
+						</div>
+
+						<div className='space-y-2'>
+							<p className='italic ibm-plex-mono'>Is this a recurring event?</p>
+							<p className='ibm-plex-mono'>For now, this is a one time event. If we have enough interest, we may be able to make this recurring.</p>
+						</div>
+
+						<div className='space-y-2'>
+							<p className='italic ibm-plex-mono'>How much does it cost to attend?</p>
+							<p className='ibm-plex-mono'>The event is free to attend.</p>
+						</div>
+
+						<div className='space-y-2'>
+							<p className='italic ibm-plex-mono'>Do I need to RSVP?</p>
+							<p className='ibm-plex-mono'>Yes, you need to RSVP in order to receive the Teams link.</p>
+						</div>
+
+						<div className='space-y-2'>
+							<p className='italic ibm-plex-mono'>How do I RSVP?</p>
+							<p className='ibm-plex-mono'>You can RSVP by clicking the RSVP buttons for each individual event. Or through our lu.ma calendar <a className="text-blue-800" href="https://lu.ma/omscs/">here</a>.</p>
+						</div>
+
+						</div>
+				</section>
+
 				{/* Contact Information Section */}
-				<section className='my-12'>
+				<section id='contact' className='my-12'>
 					<h2 className='text-[#a4925a] text-3xl font-semibold'>Contact Information</h2>
 					<div className='mt-4'>
 						<p>If you have any inquiries or need further information, please don&apos;t hesitate to contact us</p>
-						<ul className='list-disc pl-6'>
-							<li className=''>
-								<a href='mailto:zaxel3@gatech.edu' className='btn bg-[#013057] mt-4 text-white hover:text-blue-600 p-4 text-center text-[0.7rem]'>
-									Zack Axel (Speaker Series Administrator)
-								</a>
-							</li>
-							<li className=''>
-								<a href='mailto:pkhazaeepoul3@gatech.edu' className='btn bg-[#013057] mt-4 text-white hover:text-blue-600 p-4 text-center text-[0.7rem]'>
-									Parsa Khazaeepoul (Website and Events Page Administrator)
-								</a>
-							</li>
+						<ul className='list-disc space-x-4'>
+							<a href='mailto:zaxel3@gatech.edu' className='btn bg-[#013057] mt-4 text-white hover:text-blue-600 p-4 text-center text-[0.7rem]'>
+								Zack Axel (Speaker Series Administrator)
+							</a>
+
+							<a href='mailto:pkhazaeepoul3@gatech.edu' className='btn bg-[#013057] mt-4 text-white hover:text-blue-600 p-4 text-center text-[0.7rem]'>
+								Parsa Khazaeepoul (Website and Events Page Administrator)
+							</a>
+
 							{/* Add more officers if necessary */}
 						</ul>
 					</div>
 				</section>
 
 				{/* Footer */}
-				<footer className='my-12 text-center text-sm'>
+				{/* <footer className='my-12 text-center text-sm'>
 					<p>© 2023 Georgia Tech OMSCS. All rights reserved.</p>
-				</footer>
+				</footer> */}
 			</div>
 		</>
 	);
