aec668e6da0daa29db59130e07ac136b844fcf41
parsasstudio <parsak@uw.edu>
2024-01-17 13:44:05 -0800

    updated website to include recording links

--- src/components/SpeakerCards.tsx
+++ src/components/SpeakerCards.tsx
@@ -6,6 +6,7 @@ interface SpeakerBioProps {
 	dateTime: string;
 	description: string;
 	rsvpLink: string;
+	recordingLink: string;
 	turingAwardWinner: boolean;
 	speakerPhoto: string;
 	turingLink?: string;
@@ -14,7 +15,7 @@ interface SpeakerBioProps {
 	// include other props as needed
 }
 
-const SpeakerBio = ({ name, dateTime, description, rsvpLink, turingAwardWinner, speakerPhoto, turingLink, talkTitle, isoDate }: SpeakerBioProps) => {
+const SpeakerBio = ({ name, dateTime, description, rsvpLink, turingAwardWinner, speakerPhoto, turingLink, talkTitle, isoDate, recordingLink }: SpeakerBioProps) => {
 	const today = new Date();
 	function daysUntilTalk(isoDate: string) {
 		const talkDate = new Date(isoDate);
@@ -37,7 +38,7 @@ const SpeakerBio = ({ name, dateTime, description, rsvpLink, turingAwardWinner, 
 							<h3 className='text-2xl font-semibold'>{name}</h3>
 							<p className='mt-1'>{dateTime}</p>
 							<p className='mt-1'>
-								<a target='_blank' rel='noreferrer' className='text-blue-500' href={turingLink}>
+								<a target='_blank' rel='noreferrer' className='text-blue-500 underline ibm-plex-mono' href={turingLink}>
 									Turing Award Recipient
 								</a>
 							</p>
@@ -45,16 +46,18 @@ const SpeakerBio = ({ name, dateTime, description, rsvpLink, turingAwardWinner, 
 					</div>
 					<p className='mt-2 text-[1.1rem] italic'>
 						&apos;{talkTitle}&apos;
-						<span className='font-bold text-blue-500'>
-							{' '}
-							{daysUntilTalk(isoDate) < 0 ? 'has concluded.' : 'starts in' + ' ' + daysUntilTalk(isoDate) + ' days.'}
-						</span>{' '}
+						<span className='font-bold text-blue-500'> {daysUntilTalk(isoDate) <= 0 ? 'has concluded.' : 'starts in' + ' ' + daysUntilTalk(isoDate) + ' days.'}</span>{' '}
 					</p>
 					<p className='mt-2 text-[1rem]'>{description}</p>
 				</div>
 
-				<a target='_blank' rel='noreferrer' href={rsvpLink} className='floating-text bg-[#013057] mt-4 font-bold text-white  hover:text-slate-500 p-4 text-center text-[1.2rem]'>
-					RSVP NOW
+				<a
+					target='_blank'
+					rel='noreferrer'
+					href={daysUntilTalk(isoDate) > 0 ? rsvpLink : recordingLink}
+					className='floating-text bg-[#013057] mt-4 font-bold text-white  hover:text-slate-500 p-4 text-center text-[1.2rem]'
+				>
+					{daysUntilTalk(isoDate) > 0 ? 'RSVP NOW' : 'WATCH NOW'}
 				</a>
 			</div>
 		);
@@ -75,16 +78,22 @@ const SpeakerBio = ({ name, dateTime, description, rsvpLink, turingAwardWinner, 
 				</div>
 				<p className='mt-2 text-[1.1rem] italic'>
 					&apos;{talkTitle}&apos;
-					<span className='font-bold text-blue-500'>
-						{' '}
-						{daysUntilTalk(isoDate) < 0 ? 'has concluded.' : 'starts in' + ' ' + daysUntilTalk(isoDate) + ' days.'}
-					</span>{' '}
+					<span className='font-bold text-blue-500'> {daysUntilTalk(isoDate) <= 0 ? 'has concluded.' : 'starts in' + ' ' + daysUntilTalk(isoDate) + ' days.'}</span>{' '}
 				</p>
 				<p className='mt-2 text-[1rem]'>{description}</p>
 			</div>
 
-			<a target='_blank' rel='noreferrer' href={rsvpLink} className='floating-text bg-[#013057] mt-4 font-bold text-white  hover:text-slate-500 p-4 text-center text-[1.2rem]'>
-				RSVP NOW
+			<a
+				target='_blank'
+				rel='noreferrer'
+				href={daysUntilTalk(isoDate) > 0 ? rsvpLink : recordingLink}
+				className={
+					daysUntilTalk(isoDate) > 0
+						? 'floating-text bg-[#013057] mt-4 font-bold text-white  hover:text-slate-500 p-4 text-center text-[1.2rem]'
+						: 'floating-text bg-[#7b9cb71f] mt-4 font-bold text-[#013057]  hover:text-slate-500 p-4 text-center text-[1.2rem]'
+				}
+			>
+				{daysUntilTalk(isoDate) > 0 && !recordingLink.startsWith('https') ? 'RSVP NOW' : 'WATCH NOW'}
 			</a>
 		</div>
 	);
